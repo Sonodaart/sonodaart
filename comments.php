6a3371ab75dc861258f6620b44a8e245d39940b7
Sonodaart <32108791+Sonodaart@users.noreply.github.com>
2017-09-25 16:07:30 +0200

    Create comments.php

---
+++ comments.php
@@ -0,0 +1,11 @@
+<html xmlns=”http://www.w3.org/1999/xhtml”&gt;
+<head>
+	<title>Prechat</title>
+</head>
+<body>
+	<form action="chat.php" method="POST">
+Inserire un nome <input type="text" name="nome" /><br />
+	<input type="submit" value="Entra"/><br />
+	</form>
+</body>
+</html>
